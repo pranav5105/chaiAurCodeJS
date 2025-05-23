696a1be6689e0fef492cccabab0c7343ed49078c
pranav5105 <pranavchougule5105@gmail.com>
2025-05-23 14:33:36 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3) //we are using nodejs,not browser
+
+//number =>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+      //symbol
+
+      let name="hitesh";
+      console.log(typeof("hitesh"));
